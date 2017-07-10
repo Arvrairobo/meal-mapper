39e15bbac47c55c7f7a260254b60b9f3d0813029
Matthew Sells <mattms@tuta.io>
2017-07-09 20:55:25 -0500

    Building Reach elements

--- app/components/Main.js
+++ app/components/Main.js
@@ -5,7 +5,24 @@ var Main = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<h1>This is a test!</h1>
+				<nav>
+					<div className='nav-wrapper'>
+						<a href="#" className="brand-logo">Meal Planner</a>
+						<ul id='nav-mobile' className='right hide-on-med-and-down'>
+							<li><a href='#'>My Dashboard</a></li>
+							<li><a href='#'>Meal Plan</a></li>
+							<li><a href='#'>Log Out</a></li>
+						</ul>
+					</div>
+				</nav>
+
+				<div className='row'>
+					<div className='col s9' id='meal-plan'>
+					</div>
+
+					<div className='col s3' id='recipe-search'>
+					</div>
+				</div>
 			</div>
 		)
 	}
