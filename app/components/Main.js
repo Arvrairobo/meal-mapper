f756d15bdfcc81359cdc0155932cbfe78b8d7713
Matthew Sells <mattms@tuta.io>
2017-07-10 19:01:23 -0500

    Removed old files

--- app/components/Main.js
+++ app/components/Main.js
@@ -8,6 +8,16 @@ var Search = require("./children/Search");
 var Main = React.createClass({
 	render: function() {
 		return (
+			<nav>
+				<div className='nav-wrapper'>
+					<a href="#" className="brand-logo">Meal Planner</a>
+					<ul id='nav-mobile' className='right hide-on-med-and-down'>
+						<li><a href='#'>My Dashboard</a></li>
+						<li><a href='#'>Meal Plan</a></li>
+						<li><a href='#'>Log Out</a></li>
+					</ul>
+				</div>
+			</nav>
 			
 			<div className='row'>
 				<div className='col s9' id='meal-plan'>
