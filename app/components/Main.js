4b5cb7e4d245e9026cf62f74b5ff91b209dd6a06
Matthew Sells <mattms@tuta.io>
2017-07-10 20:05:20 -0500

    Building search functions

--- app/components/Main.js
+++ app/components/Main.js
@@ -6,26 +6,40 @@ var Planner = require("./children/Planner");
 var Search = require("./children/Search");
 
 var Main = React.createClass({
+
+	getInitialState: function(){
+		return {
+			searchTerm: ''
+		}
+	},
+
+	setSearch: function(newSearch){
+		this.setState({ searchTerm: newSearch });
+		console.log('new search: ' + this.state.searchTerm);
+	},
+
 	render: function() {
 		return (
-			<nav>
-				<div className='nav-wrapper'>
-					<a href="#" className="brand-logo">Meal Planner</a>
-					<ul id='nav-mobile' className='right hide-on-med-and-down'>
-						<li><a href='#'>My Dashboard</a></li>
-						<li><a href='#'>Meal Plan</a></li>
-						<li><a href='#'>Log Out</a></li>
-					</ul>
-				</div>
-			</nav>
-			
-			<div className='row'>
-				<div className='col s9' id='meal-plan'>
-					<Planner />
-				</div>
+			<div>
+				<nav>
+					<div className='nav-wrapper'>
+						<a href="#" className="brand-logo">Meal Planner</a>
+						<ul id='nav-mobile' className='right hide-on-med-and-down'>
+							<li><a href='#'>My Dashboard</a></li>
+							<li><a href='#'>Meal Plan</a></li>
+							<li><a href='#'>Log Out</a></li>
+						</ul>
+					</div>
+				</nav>
+				
+				<div className='row'>
+					<div className='col s9' id='meal-plan'>
+						<Planner />
+					</div>
 
-				<div className='col s3' id='recipe-search'>
-					<Search />
+					<div className='col s3' id='recipe-search'>
+						<Search setSearch={this.setSearch} />
+					</div>
 				</div>
 			</div>
 		)
--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -1,10 +1,28 @@
 var React = require('react');
 
 var Search = React.createClass({
+
+	getInitialState: function(){
+		return {
+			searchTerm: 'chicken'
+		}
+	},
+
+	changeSearch: function(event){
+		console.log('change search');
+		this.setState({ searchTerm: event.target.value });
+	},
+
+	sendSearch: function(event){
+		console.log(this.state.searchTerm);
+		this.props.setSearch(this.state.searchTerm);
+	},
+
 	render: function(){
 		return (
 			<div>
-				Search
+				<input value={this.state.searchTerm} type='text' onChange={this.changeSearch} />
+				<a className="waves-effect waves-light btn" onClick={this.sendSearch}>Search</a>
 			</div>
 		)
 	}
