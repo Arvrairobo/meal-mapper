b6b17b7eef7a354471ea2f74816f2537de31f719
Matthew Sells <mattms@tuta.io>
2017-07-22 19:55:57 -0500

    Col styling

--- public/js/day-sizer.js
+++ public/js/day-sizer.js
@@ -1,10 +1,10 @@
 (function(){
 	var h = $(window).height();
-	$('.recipe-area').css('max-height', h - 315);
+	$('.recipe-area').css('max-height', h - 445);
 	$('.search-results').css('max-height', h - 200);
 	$(window).resize(function(){
 		var h = $(window).height();
-		$('.recipe-area').css('max-height', h - 315);
+		$('.recipe-area').css('max-height', h - 445);
 		$('.search-results').css('max-height', h - 200);
 	});
 })();
