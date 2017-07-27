71e6332adcbf4d269ddecac340eb386d250139d5
Matthew Sells <mattms@tuta.io>
2017-07-27 10:57:30 -0500

    Added search bar slide

--- public/js/search-slide.js
+++ public/js/search-slide.js
@@ -1,30 +1,44 @@
 // Slide search bar in and out when icon is clicked
 (function(){
 	var showSearch = false;
+	var currX = -280;
+	var targX = -280;
+	var moveX = 0;
+	var xIntr;
+	var width = $(window).width();
+
+	$('#search-bar').css('right', currX);
+
 	$('#pull-search').on('click', function(){
 		showSearch = !showSearch;
-		toggleBar();
-	})
-
-	function toggleBar(){
-		var width = $(window).width();
 		if(showSearch){
-			$('#search-bar').css('right', 0);
-			$('nav').css('width', width-280);
-			$('.app-wrapper').css('width', width-320);
+			targX = 0;
+			moveX = 3;
 		} else {
-			$('#search-bar').css('right', -280);
-			$('nav').css('width', width);
-			$('.app-wrapper').css('width', width);
+			targX = -280;
+			moveX = -3;
 		}
+		xIntr = setInterval(resize, 20);
+	});
+
+	function resize(){
+		if(currX < targX){
+			moveX < 12 ? moveX += .5 : null;
+			currX += moveX;
+			if(currX + moveX > targX){
+				currX = targX;
+				clearInterval(xIntr);
+			}
+		} else {
+			moveX > -12 ? moveX -= .5 : null;
+			currX += moveX;
+			if(currX + moveX < targX){
+				currX = targX;
+				clearInterval(xIntr);
+			}
+		}
+		$('#search-bar').css('right', currX);
+		$('nav').css('width', width-280+(-1*currX));
+		$('.app-wrapper').css('width', width-280+(-1*currX));
 	}
-	toggleBar();
-
-	
-
-	/* Search slide pseudo
-	1. on click, set target x and direction of slide
-	2. set interval to constantly change x and 
-
-	 */
 })();
